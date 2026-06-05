30e9593f01e70715c012860bb5ba56aaee70fc6d
ChrisBlues <chris.blues@free.fr>
2026-06-05 18:03:00 +0200

    Commit final de Cozynestt.

--- js/script.js
+++ js/script.js
@@ -98,3 +98,16 @@ function sliderInitial($sliderContainer) {
 for (let i = 0; i < $sliderContainers.length; i++) {
     sliderInitial($sliderContainers[i]);
 }
+
+// Détection du bas de page pour les remerciements au concepteur
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY + window.innerHeight;
+  const pageHeight = document.documentElement.scrollHeight;
+  const footer = document.getElementById('thank-you-footer');
+
+  if (scrollPosition >= pageHeight - 50) {
+    footer.classList.add('visible');
+  } else {
+    footer.classList.remove('visible');
+  }
+});
